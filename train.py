ee48c17660463bc05684f1bcc2e4cf777bb31a78
Rivendile <908636821@qq.com>
2023-10-17 13:06:12 +0800

    sync

--- train.py
+++ train.py
@@ -166,7 +166,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--net', type=str, required=True, help='net type')
     parser.add_argument('--gpu', action='store_true', default=False, help='use gpu or not')
-    parser.add_argument('-b', type=int, default=128, help='batch size for dataloader')
+    parser.add_argument('-b', type=int, default=8, help='batch size for dataloader')
     parser.add_argument('--epoch', type=int, default=100, help='num of epochs to train')
     parser.add_argument('--warm', type=int, default=1, help='warm up training phase')
     parser.add_argument('--lr', type=float, default=0.1, help='initial learning rate')
